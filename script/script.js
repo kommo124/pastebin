3564001493cf0e293b917ed880533aea6b4462de
kommo124 <kommo124@users.noreply.github.com>
2026-04-19 20:17:34 +0300

    func loadData() to display all data from db on website

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('mainForm')
 const postButton = document.getElementById('toDb')
+const getBtn = document.getElementById('getALl')
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
@@ -14,13 +15,11 @@ form.addEventListener('submit', async (e) => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
-
             },
 
             body: JSON.stringify(data)
         })
 
-        
         const resultData = await responce.json()
         if (responce.ok) {
             console.log('Данные занесены в базу: ', resultData)
@@ -31,6 +30,31 @@ form.addEventListener('submit', async (e) => {
         } catch(error) {
             console.error(error)
         }
-        
-
 })
+
+async function loadData() {
+    
+    try {
+        const responce = await fetch("http://127.0.0.1:8000/data")
+        const data = await responce.json()
+
+        const outputContainer = document.getElementById('output')
+        outputContainer.innerHTML = ''
+
+        data.forEach(item => {
+            const div = document.createElement('div')
+            div.textContent = `Текст: ${item.text}, Тэг: ${item.tag}`
+            outputContainer.appendChild(div)
+        
+           
+        });
+    } catch (error) {
+        console.error(error)
+    }
+
+    
+
+    
+}
+
+
